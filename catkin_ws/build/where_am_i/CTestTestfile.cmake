216af56e47f1410a4e56a149709e2f396a931082
robond <stephen_dong_127@hotmail.com>
2020-09-23 23:57:23 -0500

    adding catkin_ws

---
+++ catkin_ws/build/where_am_i/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/robond/catkin_ws/src/where_am_i
+# Build directory: /home/robond/catkin_ws/build/where_am_i
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
