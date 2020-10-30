5cda2fba6b31f726862034add49cdced161030f7
jw-dong <stephen_dong_127@hotmail.com>
2020-10-30 21:57:22 +0000

    Project home service robot

---
+++ 6 Home Service Robot/scripts/home_service.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/src/map/my_world.world" &
+sleep 5
+
+xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/src/map/map.yaml" &
+sleep 5
+
+xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5
+
+xterm -e "roslaunch pick_objects pick_objects.launch" &
+sleep 5
+
+xterm -e "roslaunch add_markers add_markers.launch"
+
