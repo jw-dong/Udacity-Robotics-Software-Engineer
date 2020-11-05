152c95d52a57f832daa25f58b1539552a656dbe8
Jiawei Dong <stephen_dong_127@hotmail.com>
2020-11-05 11:00:26 -0600

    update files

--- 6 Home Service Robot/scripts/add_markers.sh
+++ 6 Home Service Robot/scripts/add_markers.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/src/map/my_world.world" &
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find map)/map/my_world.world" &
 sleep 5
 
-xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/src/map/map.yaml" &
+xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(rospack find map)/map/map.yaml" &
 sleep 5
 
 xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
